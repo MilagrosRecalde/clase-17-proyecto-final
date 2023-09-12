e2faaac9a3c320e3fcefcc6e35adb2ea546cc824
Milagros Recalde <recaldemilagros99@gmail.com>
2023-09-11 23:34:12 -0300

    Versión final del proyecto

---
+++ app.js
@@ -0,0 +1,34 @@
+const contenedor = document.getElementById ("contenedor")
+
+const personajes = fetch("https://rickandmortyapi.com/api/character")
+
+
+personajes.then((response) => {
+    return response.json()
+}).then((data) => {
+    const arrayDePersonajes = data.results
+
+    for(let i = 0; i < arrayDePersonajes.length; i++) {
+        const imagen = arrayDePersonajes[i].image
+        const nombre = arrayDePersonajes[i].name
+        const genero = arrayDePersonajes[i].gender
+        const specie = arrayDePersonajes[i].species
+        const estado = arrayDePersonajes[i].status
+
+        contenedor.innerHTML += 
+        `
+        <div class="cont-personajes">
+            <img 
+            src= "${imagen}"
+             alt="Foto del personaje"
+             />
+        <h3>${nombre}</h3>
+            <p>Género: ${genero}</p>
+            <p>Especie: ${specie}</p>
+            <p>Estado: ${estado}</p>
+        </div>
+        `
+
+    }
+
+})
